2da9e6ced4a2e1def65f3123c086607298608ac2
Riccardo <riccardozaupa02@gmail.com>
2023-11-15 15:21:32 +0100

    Primo esercizio cartella 6 risolto

--- 6. Web Intro/3_das_blog/docker_run.sh
+++ 6. Web Intro/3_das_blog/docker_run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 docker run "$@" --rm -p "127.0.0.1:8084:80" --name "2018_das_blog" -it "2018_das_blog"
+
+
+JohnsTestUser        AT3stAccountForT3sting
--- 7. Ingredients of Web/1_ajax_not_borax/docker_run.sh
+++ 7. Ingredients of Web/1_ajax_not_borax/docker_run.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 docker run "$@" --rm -p "127.0.0.1:8083:80" --name "ajax_not_borax" -it "zenhackteam/ajax_not_borax"
+$('#output').html('Username is correct');
+
+y.trigger(l ? 'ajaxSuccess' : 'ajaxError', [
+              E,
+              h,
+              l ? p : d
+            ]),
+
+
+            y.trigger('ajaxComplete', [
+                E,
+                h
+              ]),
+
+
+              var Y = function (e) {
+      return 1 === e.nodeType ||
+      9 === e.nodeType ||
+      ! + e.nodeType
+    };
